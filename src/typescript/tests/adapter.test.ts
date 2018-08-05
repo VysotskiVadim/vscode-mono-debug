624d151aff3b98d61c6d4ee003971db76a875100
Vadzim Vysotski <vadzimv@playtika.com>
2018-08-05 21:50:06 +0300

    working on tests

--- src/typescript/tests/adapter.test.ts
+++ src/typescript/tests/adapter.test.ts
@@ -28,104 +28,104 @@ suite('Node Debug Adapter', () => {
 	teardown( () => dc.stop() );
 
 
-	suite('basic', () => {
+	// suite('basic', () => {
 
-		test('unknown request should produce error', done => {
-			dc.send('illegal_request').then(() => {
-				done(new Error("does not report error on unknown request"));
-			}).catch(() => {
-				done();
-			});
-		});
-	});
+	// 	test('unknown request should produce error', done => {
+	// 		dc.send('illegal_request').then(() => {
+	// 			done(new Error("does not report error on unknown request"));
+	// 		}).catch(() => {
+	// 			done();
+	// 		});
+	// 	});
+	// });
 
-	suite('initialize', () => {
+	// suite('initialize', () => {
 
-		test('should produce error for invalid \'pathFormat\'', done => {
-			dc.initializeRequest({
-				adapterID: 'mono',
-				linesStartAt1: true,
-				columnsStartAt1: true,
-				pathFormat: 'url'
-			}).then(response => {
-				done(new Error("does not report error on invalid 'pathFormat' attribute"));
-			}).catch(err => {
-				// error expected
-				done();
-			});
-		});
-	});
+	// 	test('should produce error for invalid \'pathFormat\'', done => {
+	// 		dc.initializeRequest({
+	// 			adapterID: 'mono',
+	// 			linesStartAt1: true,
+	// 			columnsStartAt1: true,
+	// 			pathFormat: 'url'
+	// 		}).then(response => {
+	// 			done(new Error("does not report error on invalid 'pathFormat' attribute"));
+	// 		}).catch(err => {
+	// 			// error expected
+	// 			done();
+	// 		});
+	// 	});
+	// });
 
-	suite('launch', () => {
+	suite('launch android', () => {
 
-		test('should run program to the end', () => {
+		// test('should run program to the end', () => {
 
-			const PROGRAM = Path.join(DATA_ROOT, 'simple/Program.exe');
+		// 	const PROGRAM = Path.join(DATA_ROOT, 'simple/Program.exe');
 
-			return Promise.all([
-				dc.configurationSequence(),
-				dc.launch({ program: PROGRAM }),
-				dc.waitForEvent('terminated')
-			]);
-		});
+		// 	return Promise.all([
+		// 		dc.configurationSequence(),
+		// 		dc.launch({ program: PROGRAM }),
+		// 		dc.waitForEvent('terminated')
+		// 	]);
+		// });
 
-		test('should run program to the end (and not stop on Debugger.Break())', () => {
+		// test('should run program to the end (and not stop on Debugger.Break())', () => {
 
-			const PROGRAM = Path.join(DATA_ROOT, 'simple_break/Program.exe');
+		// 	const PROGRAM = Path.join(DATA_ROOT, 'simple_break/Program.exe');
 
-			return Promise.all([
-				dc.configurationSequence(),
-				dc.launch({ program: PROGRAM, noDebug: true }),
-				dc.waitForEvent('terminated')
-			]);
-		});
+		// 	return Promise.all([
+		// 		dc.configurationSequence(),
+		// 		dc.launch({ program: PROGRAM, noDebug: true }),
+		// 		dc.waitForEvent('terminated')
+		// 	]);
+		// });
 
-		test('should stop on debugger statement', () => {
+		// test('should stop on debugger statement', () => {
 
-			const PROGRAM = Path.join(DATA_ROOT, 'simple_break/Program.exe');
-			const DEBUGGER_LINE = 11;
+		// 	const PROGRAM = 'com.xamarin.debugexample.x_a_debug';
+		// 	const DEBUGGER_LINE = 16;
 
-			return Promise.all([
-				dc.configurationSequence(),
-				dc.launch({ program: PROGRAM }),
-				dc.assertStoppedLocation('step', {line:DEBUGGER_LINE})
-			]);
-		});
+		// 	return Promise.all([
+		// 		dc.configurationSequence(),
+		// 		dc.launch({ packageName: PROGRAM }),
+		// 		dc.assertStoppedLocation('step', {line:DEBUGGER_LINE})
+		// 	]);
+		// });
 	});
 
 	suite('setBreakpoints', () => {
 
-		const PROGRAM = Path.join(DATA_ROOT, 'simple/Program.exe');
-		const SOURCE = Path.join(DATA_ROOT, 'simple/Program.cs');
-		const BREAKPOINT_LINE = 13;
+		const PROGRAM = 'com.xamarin.debugexample.x_a_debug';
+		const SOURCE = Path.join(DATA_ROOT, 'xamarin_android/X.A.Debug/MainActivity.cs');
+		const BREAKPOINT_LINE = 16;
 
 		test('should stop on a breakpoint', () => {
-			return dc.hitBreakpoint({ program: PROGRAM }, { path: SOURCE, line: BREAKPOINT_LINE } );
+			return dc.hitBreakpoint({ packageName: PROGRAM }, { path: SOURCE, line: BREAKPOINT_LINE } );
 		});
 	});
 
-	suite('output events', () => {
+	// suite('output events', () => {
 
-		const PROGRAM = Path.join(DATA_ROOT, 'output/Output.exe');
+	// 	const PROGRAM = Path.join(DATA_ROOT, 'output/Output.exe');
 
-		test('stdout and stderr events should be complete and in correct order', () => {
-			return Promise.all([
-				dc.configurationSequence(),
-				dc.launch({ program: PROGRAM }),
-				dc.assertOutput('stdout', "Hello stdout 0\nHello stdout 1\nHello stdout 2\n"),
-				dc.assertOutput('stderr', "Hello stderr 0\nHello stderr 1\nHello stderr 2\n")
-			]);
-		});
-	});
+	// 	test('stdout and stderr events should be complete and in correct order', () => {
+	// 		return Promise.all([
+	// 			dc.configurationSequence(),
+	// 			dc.launch({ program: PROGRAM }),
+	// 			dc.assertOutput('stdout', "Hello stdout 0\nHello stdout 1\nHello stdout 2\n"),
+	// 			dc.assertOutput('stderr', "Hello stderr 0\nHello stderr 1\nHello stderr 2\n")
+	// 		]);
+	// 	});
+	// });
 
-	suite('FSharp Tests', () => {
+	// suite('FSharp Tests', () => {
 
-		const PROGRAM = Path.join(DATA_ROOT, 'fsharp/Program.exe');
-		const SOURCE = Path.join(DATA_ROOT, 'fsharp/Program.fs');
-		const BREAKPOINT_LINE = 8;
+	// 	const PROGRAM = Path.join(DATA_ROOT, 'fsharp/Program.exe');
+	// 	const SOURCE = Path.join(DATA_ROOT, 'fsharp/Program.fs');
+	// 	const BREAKPOINT_LINE = 8;
 
-		test('should stop on a breakpoint in an fsharp program', () => {
-			return dc.hitBreakpoint({ program: PROGRAM }, { path: SOURCE, line: BREAKPOINT_LINE } );
-		});
-	});
+	// 	test('should stop on a breakpoint in an fsharp program', () => {
+	// 		return dc.hitBreakpoint({ program: PROGRAM }, { path: SOURCE, line: BREAKPOINT_LINE } );
+	// 	});
+	// });
 });
--- testdata/xamarin_android/X.A.Debug/MainActivity.cs
+++ testdata/xamarin_android/X.A.Debug/MainActivity.cs
@@ -8,12 +8,12 @@ namespace X.A.Debug
     [Activity(Label = "X.A. Debug", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Debugger.Break();
+            
+            var a = 15;
+            var b = a + 2;
         }
     }
 }
