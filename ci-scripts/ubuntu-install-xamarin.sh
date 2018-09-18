6d82acd7809833da695c6511657e4574da9bd715
Vadzim Vysotski <vadzimv@playtika.com>
2018-09-18 15:08:30 +0300

    fixed mono installtion on ubuntu

--- ci-scripts/ubuntu-install-xamarin.sh
+++ ci-scripts/ubuntu-install-xamarin.sh
@@ -1,3 +1,7 @@
-sudo apt-get install mono msbuild libzip4
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt install apt-transport-https
+echo "deb https://download.mono-project.com/repo/ubuntu stable-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt-get install mono-complete
 wget https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/993/Azure/processDownloadRequest/xamarin-android/xamarin.android-oss_8.3.99.189_amd64.deb
 sudo dpkg -i xamarin.android-oss_8.3.99.189_amd64.deb
