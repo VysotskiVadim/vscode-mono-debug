8f7e699289748834ac800a6df0aa6ed1a4bfd3a2
Vadzim Vysotski <vadzimv@playtika.com>
2018-09-18 14:35:56 +0300

    fixed install package command

--- ci-scripts/ubuntu-install-xamarin.sh
+++ ci-scripts/ubuntu-install-xamarin.sh
@@ -1,3 +1,3 @@
 sudo apt-get install mono
 wget https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/993/Azure/processDownloadRequest/xamarin-android/xamarin.android-oss_8.3.99.189_amd64.deb
-sudo dpgk -i xamarin.android-oss_8.3.99.189_amd64.deb
+sudo dpkg -i xamarin.android-oss_8.3.99.189_amd64.deb
