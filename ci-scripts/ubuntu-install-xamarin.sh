c1ab227e9abc45b324e01c44349d2e876906b586
Vadzim Vysotski <vadzimv@playtika.com>
2018-09-18 14:52:25 +0300

    install xamarin android dependencies

--- ci-scripts/ubuntu-install-xamarin.sh
+++ ci-scripts/ubuntu-install-xamarin.sh
@@ -1,3 +1,3 @@
-sudo apt-get install mono
+sudo apt-get install mono msbuild libzip4
 wget https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/993/Azure/processDownloadRequest/xamarin-android/xamarin.android-oss_8.3.99.189_amd64.deb
 sudo dpkg -i xamarin.android-oss_8.3.99.189_amd64.deb
