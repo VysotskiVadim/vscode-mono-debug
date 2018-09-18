af8ce871ebd52f334825250713d600b48ca489b5
Vadzim Vysotski <vadzimv@playtika.com>
2018-09-18 15:21:30 +0300

    + libzip4 for ubuntu

--- ci-scripts/ubuntu-install-xamarin.sh
+++ ci-scripts/ubuntu-install-xamarin.sh
@@ -2,6 +2,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 sudo apt install apt-transport-https
 echo "deb https://download.mono-project.com/repo/ubuntu stable-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
-sudo apt-get install mono-complete
+sudo apt-get install mono-complete libzip4
 wget https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/993/Azure/processDownloadRequest/xamarin-android/xamarin.android-oss_8.3.99.189_amd64.deb
 sudo dpkg -i xamarin.android-oss_8.3.99.189_amd64.deb
