9f50e0632bbfe98d94819450570e955aa722e45c
Vadzim Vysotski <vadzimv@playtika.com>
2018-09-19 17:19:23 +0300

    try to set android sdk install directory explisitly

--- ci-scripts/install-android-sdk.sh
+++ ci-scripts/install-android-sdk.sh
@@ -1,10 +1,10 @@
 curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$ANDROID_COMPONENTS --accept=$ANDROID_LICENSES
-source ~/.android-sdk-installer/env
-export ANDROID_SDK_ROOT=~/.android-sdk-installer/android-sdk-macosx
+source ~/.android-sdk-installer/env --dir=$HOME/.android-sdk-installer
+export ANDROID_SDK_ROOT=~/.android-sdk-installer
 # Create and start emulator
 android list target
 android list sdk --extended -a
 #data for troubleshoot android emulator start
-ls -la ~/.android-sdk-installer/android-sdk-macosx/system-images/android-24
-ls -la ~/.android-sdk-installer/android-sdk-macosx/system-images/android-24/default
-ls -la ~/.android-sdk-installer/android-sdk-macosx/system-images/android-24/default/armeabi-v7a
+ls -la $ANDROID_SDK_ROOT/system-images/android-24
+ls -la $ANDROID_SDK_ROOT/system-images/android-24/default
+ls -la $ANDROID_SDK_ROOT/system-images/android-24/default/armeabi-v7a
