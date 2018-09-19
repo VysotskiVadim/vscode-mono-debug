be970f6434c5a935bb6398569cf41cc9f65f44fc
Vadzim Vysotski <vadzimv@playtika.com>
2018-09-19 17:29:08 +0300

    more logs

--- ci-scripts/install-android-sdk.sh
+++ ci-scripts/install-android-sdk.sh
@@ -1,10 +1,10 @@
 curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$ANDROID_COMPONENTS --accept=$ANDROID_LICENSES
 source ~/.android-sdk-installer/env --dir=$HOME/.android-sdk-installer
 export ANDROID_SDK_ROOT=~/.android-sdk-installer
-# Create and start emulator
-android list target
-android list sdk --extended -a
-#data for troubleshoot android emulator start
+#data for troubleshooting 
+#android list target
+#android list sdk --extended -a
+ls -la $ANDROID_SDK_ROOT
 ls -la $ANDROID_SDK_ROOT/system-images/android-24
 ls -la $ANDROID_SDK_ROOT/system-images/android-24/default
 ls -la $ANDROID_SDK_ROOT/system-images/android-24/default/armeabi-v7a
