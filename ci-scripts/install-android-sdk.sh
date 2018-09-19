43669fa59619fdf1907155dd45007f33e42466e9
Vadzim Vysotski <vadzimv@playtika.com>
2018-09-19 17:36:05 +0300

    fixed sdk path

--- ci-scripts/install-android-sdk.sh
+++ ci-scripts/install-android-sdk.sh
@@ -1,6 +1,15 @@
 curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$ANDROID_COMPONENTS --accept=$ANDROID_LICENSES
-source ~/.android-sdk-installer/env --dir=$HOME/.android-sdk-installer
-export ANDROID_SDK_ROOT=~/.android-sdk-installer
+source ~/.android-sdk-installer/env
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]
+then 
+    export ANDROID_SDK_ROOT=~/.android-sdk-installer/android-sdk-macosx
+fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]
+then 
+    export ANDROID_SDK_ROOT=~/.android-sdk-installer/android-sdk-linux
+fi
+
 #data for troubleshooting 
 #android list target
 #android list sdk --extended -a
