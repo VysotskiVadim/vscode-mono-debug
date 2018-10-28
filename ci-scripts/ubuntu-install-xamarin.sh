1276090cf158eaba2d53d3840dd55845df4d6bfa
vadzimv <vysotski.vadim@gmail.com>
2018-10-28 17:36:39 +0300

    install jdk 8

--- ci-scripts/ubuntu-install-xamarin.sh
+++ ci-scripts/ubuntu-install-xamarin.sh
@@ -1,3 +1,11 @@
+# install oracle jdk 8
+sudo apt-get update
+sudo apt-get install -y software-properties-common debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 sudo apt install apt-transport-https
 echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
